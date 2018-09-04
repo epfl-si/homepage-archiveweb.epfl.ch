f5ad338002a43bc89231fc1e99642dc8dde79718
obieler <olivier.bieler@epfl.ch>
2018-09-04 16:52:37 +0200

    initial implementation

--- archived-websites.js
+++ archived-websites.js
@@ -1,30 +1,27 @@
-require(["epfl-jquery"], function($){
-  "use strict";
+"use strict";
 
-  $(function() {
-    // Load json file + add websites in page....
-    $.ajax({
-      type: 'GET',
-      url: 'archived-websites.json',
-      timeout: 3000,
-      dataType: 'text',
-      success: function(data) {
-        var catalogue = JSON.parse(data);
-        catalogue.sort();
-        $.each(catalogue,function(index, value) {
-          var ul = document.getElementById("list-archived-websites");
-          var li = document.createElement("li");
-          var a = document.createElement("a");
-          a.href = ("http://" + value);
-          a.innerHTML = (value);
-          li.appendChild(a);
-          ul.appendChild(li);
-        });
-      },
-      error: function() {
-        alert('La requête n\'a pas abouti');
-      }
-    });
+$(function() {
+  // Load json file + add websites in page....
+  $.ajax({
+    type: 'GET',
+    url: 'archived-websites.json',
+    timeout: 3000,
+    dataType: 'text',
+    success: function(data) {
+      var catalogue = JSON.parse(data);
+      catalogue.sort();
+      $.each(catalogue,function(index, value) {
+        var ul = document.getElementById("list-archived-websites");
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        a.href = ("http://" + value);
+        a.innerHTML = (value);
+        li.appendChild(a);
+        ul.appendChild(li);
+      });
+    },
+    error: function() {
+      alert('La requête n\'a pas abouti');
+    }
   });
-
 });
