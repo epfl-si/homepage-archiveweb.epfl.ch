fc49b1165648685bc0cb7e9d348e4aa131fdd2f3
William Belle <william.belle@gmail.com>
2020-10-05 09:22:11 +0200

    Add support of Feather icons (#50)
    Close: #49

--- bin/archive-wp-lab.sh
+++ bin/archive-wp-lab.sh
@@ -16,6 +16,7 @@ LAB_PATH=www.epfl.ch/labs/${LAB_NAME}
 
 ICONS_PATH=wp-content/themes/wp-theme-2018/assets/icons
 ICONS_URL=${LAB_URL}${ICONS_PATH}/icons.svg
+FEATHER_URL=${LAB_URL}${ICONS_PATH}/feather-sprite.svg
 
 LOGO_PATH=wp-content/themes/wp-theme-2018/assets/svg/
 LOGO_URL=${LAB_URL}${LOGO_PATH}/epfl-logo.svg
@@ -24,6 +25,9 @@ ARCHIVE_URL=https://archiveweb.epfl.ch
 
 ICONS_BF="window.svgPath = \"${ICONS_URL}\""
 ICONS_AF="window.svgPath = \"${ARCHIVE_URL}/${LAB_NAME}.epfl.ch/${ICONS_PATH}/icons.svg\""
+
+FEATHER_BF="window.featherSvgPath = \"${FEATHER_URL}\""
+FEATHER_AF="window.featherSvgPath = \"${ARCHIVE_URL}/${LAB_NAME}.epfl.ch/${ICONS_PATH}/feather-sprite.svg\""
 
 ARCHIVES_DIR=archives
 
@@ -34,10 +38,14 @@ cd $ARCHIVES_DIR
 mirror-website.sh "${LAB_URL}"
 
 wget -q --show-progress ${ICONS_URL} -P ${LAB_PATH}/${ICONS_PATH}
+wget -q --show-progress ${FEATHER_URL} -P ${LAB_PATH}/${ICONS_PATH}
 wget -q --show-progress ${LOGO_URL} -P ${LAB_PATH}/${LOGO_PATH}
 
 echo "\nReplacing icons url"
 find-and-replace-string.py "${ICONS_BF}" "${ICONS_AF}"
+
+echo "\nReplacing feather icons url"
+find-and-replace-string.py "${FEATHER_BF}" "${FEATHER_AF}"
 
 echo "\nReplacing EPFL logo url"
 replace-epfl-logo-url.sh
