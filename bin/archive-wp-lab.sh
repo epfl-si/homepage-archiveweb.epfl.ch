3efff0488a5e02aaa1716860db511f04a3c26ed6
William Belle <william.belle@gmail.com>
2020-12-24 10:58:36 +0100

    Fix archive-wp-lab

--- bin/archive-wp-lab.sh
+++ bin/archive-wp-lab.sh
@@ -10,16 +10,20 @@ die() {
 
 [ -n "$1" ] || die "Please provide the name of a lab."
 
+# https://github.com/epfl-si/wp-ops/blob/master/ansible/vars/wordpress-vars.yml
+WP_VERSION=5.5
+WP_THEME_PATH=wp/${WP_VERSION}/wp-content/themes/wp-theme-2018
+
 LAB_NAME=$1
 LAB_URL=https://www.epfl.ch/labs/${LAB_NAME}/
 LAB_PATH=www.epfl.ch/labs/${LAB_NAME}
 
-ICONS_PATH=wp-content/themes/wp-theme-2018/assets/icons
-ICONS_URL=${LAB_URL}${ICONS_PATH}/icons.svg
-FEATHER_URL=${LAB_URL}${ICONS_PATH}/feather-sprite.svg
+ICONS_PATH=${WP_THEME_PATH}/assets/icons
+ICONS_URL=https://www.epfl.ch/${ICONS_PATH}/icons.svg
+FEATHER_URL=https://www.epfl.ch/${ICONS_PATH}/feather-sprite.svg
 
-LOGO_PATH=wp-content/themes/wp-theme-2018/assets/svg/
-LOGO_URL=${LAB_URL}${LOGO_PATH}/epfl-logo.svg
+LOGO_PATH=${WP_THEME_PATH}/assets/svg/
+LOGO_URL=https://www.epfl.ch/${LOGO_PATH}/epfl-logo.svg
 
 ARCHIVE_URL=https://archiveweb.epfl.ch
 
@@ -28,6 +32,9 @@ ICONS_AF="window.svgPath = \"${ARCHIVE_URL}/${LAB_NAME}.epfl.ch/${ICONS_PATH}/ic
 
 FEATHER_BF="window.featherSvgPath = \"${FEATHER_URL}\""
 FEATHER_AF="window.featherSvgPath = \"${ARCHIVE_URL}/${LAB_NAME}.epfl.ch/${ICONS_PATH}/feather-sprite.svg\""
+
+PATH_THEME_BF="../../wp/"
+PATH_THEME_AF="./wp/"
 
 ARCHIVES_DIR=archives
 
@@ -47,9 +54,16 @@ find-and-replace-string.py "${ICONS_BF}" "${ICONS_AF}"
 echo "\nReplacing feather icons url"
 find-and-replace-string.py "${FEATHER_BF}" "${FEATHER_AF}"
 
+echo "\nReplacing path theme"
+find-and-replace-string.py "${PATH_THEME_BF}" "${PATH_THEME_AF}"
+
+echo "\nReplacing path fonts"
+find-and-replace-string.py "../../../../../../fonts/" "/fonts/"
+
 echo "\nReplacing EPFL logo url"
 replace-epfl-logo-url.sh
 
 mv www.epfl.ch/labs/${LAB_NAME} ${LAB_NAME}.epfl.ch
+cp -r www.epfl.ch/wp ${LAB_NAME}.epfl.ch
 
 rm -fr www.epfl.ch
