52e138d1f00f8e7e5126809ac90a1a5b5ddeb668
William Belle <william.belle@gmail.com>
2022-06-28 10:45:59 +0200

    Do not check the server certificate (#84)
    Close: #82

--- bin/mirror-website.sh
+++ bin/mirror-website.sh
@@ -10,4 +10,4 @@ die() {
 
 [ -n "$1" ] || die "Please provide a url."
 
-wget -p -k -E -m -e robots=off -w 1 --no-parent -q --show-progress $1
+wget -p -k -E -m -e robots=off -w 1 --no-parent -q --no-check-certificate --show-progress $1
