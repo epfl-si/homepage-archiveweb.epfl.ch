e74719172b91fe3b196285f762ee1745c69b937f
William Belle <william.belle@gmail.com>
2022-07-04 08:31:46 +0200

    Migrate `fill-archivedwebsites-json.py` to Python 3 (#88)
    Close: #87

--- bin/fill-archivedwebsites-json.py
+++ bin/fill-archivedwebsites-json.py
@@ -1,15 +1,15 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# Fill a json file of archived websites (folders found in /var/www/vhosts/archivedweb.epfl.ch/htdocs/)
+#! /usr/bin/env python3
+
+# Fill a json file of archived websites (folders found in /svc0176_archive_web_app/archiveweb/)
 import json
-import os, time
+import os
 
 def listdirs(folder):
     return [d for d in os.listdir(folder) if os.path.isdir(os.path.join(folder, d))]
 
-print 'Run fill-archivedwebsites-json.py...'
+print('Run fill-archivedwebsites-json.py...')
 
-directories = listdirs('/var/www/vhosts/archiveweb.epfl.ch/htdocs/')
+directories = listdirs('/svc0176_archive_web_app/archiveweb/')
 
 directories_to_remove = ['common', 'templates', 'fonts', 'cdn-cgi']
 
@@ -17,10 +17,10 @@ directories[:] = [item for item in directories if item not in directories_to_rem
 
 websites_list = json.dumps(directories)
 
-print 'Number of archived websites = ', len(directories)
+print('Number of archived websites = ', len(directories))
 
-file = open('/var/www/vhosts/archiveweb.epfl.ch/htdocs/archived-websites.json','w')
+file = open('/svc0176_archive_web_app/archiveweb/archived-websites.json','w')
 file.write(websites_list)
 file.close()
 
-print 'Success!'
+print('Success!')
