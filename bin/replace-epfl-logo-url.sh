7cc30c1fb91fd32b03b09239e2fba6cac4f16b56
William Belle <william.belle@gmail.com>
2025-05-08 11:01:51 +0200

    Remove epfl-logo script
    Close: #249

--- bin/replace-epfl-logo-url.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-# (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2020.
-
-LOGO_BF="epfl-logo.svg%3Frefresh=now"
-LOGO_AF="epfl-logo.svg"
-
-find-and-replace-string.py "${LOGO_BF}" "${LOGO_AF}"
